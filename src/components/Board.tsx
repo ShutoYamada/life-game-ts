7fc101a499e14d006cabc3108be3b4a95e13bbf6
ShutoYamada <stym1122@gmail.com>
2021-01-18 16:51:23 +0900

    add State to Board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Cell from "./Cell";
 import CellInfo from "../modules/CellInfo";
+import { generateRandomActiveCellPoint } from "../utils/CellUtil";
 
 type State = {
+  // 行数
+  rowCount: number;
+  // 初期のアクティブセル数
+  defaultActiveCount: number;
+  // セル行のリスト
   cellRowList: CellInfo[][];
 };
 
@@ -11,6 +17,8 @@ class Board extends React.Component<{}, State> {
     super(props);
 
     this.state = {
+      rowCount: 8,
+      defaultActiveCount: 12,
       cellRowList: [],
     };
   }
@@ -19,17 +27,28 @@ class Board extends React.Component<{}, State> {
     this.initCellRowList();
   };
 
+  /**
+   * セル行リストを初期化
+   */
   initCellRowList = () => {
-    const n: number = 8;
+    const { rowCount, defaultActiveCount } = this.state;
+    // アクティブなセルの座標リストを乱数生成
+    const activePointList: {
+      x: number;
+      y: number;
+    }[] = generateRandomActiveCellPoint(rowCount, defaultActiveCount);
     let cellRowList: CellInfo[][] = [];
 
-    for (let i = 0; i < n; i++) {
+    // 各セルの組み立て
+    for (let i = 0; i < rowCount; i++) {
       let rowList: CellInfo[] = [];
-      for (let j = 0; j < n; j++) {
+      for (let j = 0; j < rowCount; j++) {
         let cell: CellInfo = new CellInfo();
         cell.x = i;
         cell.y = j;
-        cell.isActive = false;
+        cell.isActive = !!activePointList.find((p) => {
+          return p.x === cell.x && p.y === cell.y;
+        });
         rowList.push(cell);
       }
       cellRowList.push(rowList);
@@ -38,32 +57,30 @@ class Board extends React.Component<{}, State> {
     this.setState({
       cellRowList: cellRowList,
     });
-
-    console.log("init");
-    console.log(cellRowList);
   };
 
   render = () => {
     const { cellRowList } = this.state;
-    console.log("????");
-    console.log(cellRowList);
     return (
       <div style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <button>hoge</button>
-        {cellRowList.map((row: CellInfo[], index: number) => {
-          return (
-            <div
-              key={`row_${index}`}
-              style={{
-                display: "table",
-              }}
-            >
-              {row.map((cell: CellInfo) => {
-                return <Cell key={`cell_${cell.x}_${cell.y}`} data={cell} />;
-              })}
-            </div>
-          );
-        })}
+        <button>Start</button>
+        <div style={{ margin: "2% auto auto auto" }}>
+          {cellRowList.map((row: CellInfo[], index: number) => {
+            return (
+              <div
+                key={`row_${index}`}
+                style={{
+                  display: "table",
+                  margin: "auto",
+                }}
+              >
+                {row.map((cell: CellInfo) => {
+                  return <Cell key={`cell_${cell.x}_${cell.y}`} data={cell} />;
+                })}
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   };
