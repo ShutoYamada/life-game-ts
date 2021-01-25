1c64a8ed022fc0081a48857be7495f632c995705
ShutoYamada <stym1122@gmail.com>
2021-01-25 11:00:37 +0900

    セル情報のコンストラクタを変更

--- src/modules/CellInfo.ts
+++ src/modules/CellInfo.ts
@@ -3,10 +3,10 @@ class CellInfo {
     public y: number;
     public isActive: boolean;
 
-    public constructor() {
-        this.x = 0;
-        this.y = 0;
-        this.isActive = false;
+    public constructor(x?: number, y?: number, isActive?: boolean) {
+        this.x = x || 0;
+        this.y = y || 0;
+        this.isActive = isActive || false;
     }
 }
 
